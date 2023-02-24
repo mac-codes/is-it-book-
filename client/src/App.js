403c3d8041915b33b46fee7a64e9be82502b4bef
McKenna Lawson <mckenna.lawson01@gmail.com>
2023-02-24 11:56:39 -0600

    publish

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
+import Navbar from './components/Navbar';
 import ApolloClient from 'apollo-boost';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+
 
 // create a new ApolloClient instance
 const client = new ApolloClient({
